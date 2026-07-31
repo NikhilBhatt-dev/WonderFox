2e53b74ef746199c4d139c474dfc7cb67f412322
Nikhil Bhatt <bhattnikhil158@gmail.com>
2026-07-31 18:21:34 +0530

    Setup admin routing

---
+++ admin/src/App.jsx
@@ -0,0 +1,26 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Login from "./pages/auth/Login";
+import Dashboard from "./pages/dashboard/Dashboard";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+
+        <Route
+          path="/"
+          element={<Login />}
+        />
+
+        <Route
+          path="/dashboard"
+          element={<Dashboard />}
+        />
+
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
